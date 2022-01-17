8865d10cc24838e267776b8188c66ab0f748d6d2
yardenc2003 <yardenc2003@gmail.com>
2022-01-17 14:41:47 +0200

    Added Main class for feature testings

---
+++ src/main/java/workspace/Main.java
@@ -0,0 +1,18 @@
+package workspace;
+
+import com.opencsv.exceptions.CsvValidationException;
+import workspace.fileHandlers.readers.parsers.csv.CSVFileParser;
+import workspace.models.Entity;
+import workspace.models.madaReport.MadaReportFactory;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws CsvValidationException, IOException {
+        List<Entity> madaReportList = new CSVFileParser().extractEntities(new MadaReportFactory(), "C:\\Users\\yarde\\Documents\\Exercises\\Solutions\\MyGitProjects\\ma28-sampler\\src\\main\\resources\\MadaReports.csv");
+        Entity e = madaReportList.get(0);
+        System.out.println(e.toString());
+    }
+}
