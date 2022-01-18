a83b7282a1fec266f124273a7fd0568009c67b40
yardenc2003 <yardenc2003@gmail.com>
2022-01-18 14:04:33 +0200

    Added Main class for testing

--- src/main/java/workspace/Main.java
+++ src/main/java/workspace/Main.java
@@ -13,15 +13,25 @@ public class Main {
     public static final String configurationsFileName = "C:\\Users\\yarde\\Documents\\Exercises\\Solutions\\MyGitProjects\\ma28-sampler\\src\\main\\resources\\sampler_configurations.properties";
 
     public static void main(String[] args) throws IOException {
+        // configurations-handler initialization
         ConfigurationsHandler configurationsHandler = new PropertiesHandler();
         configurationsHandler.load(configurationsFileName);
 
+        // retrieve configuration field
         int maxRecords = Integer.parseInt(configurationsHandler.getField("maxRecords"));
+
+        // sampler-etl-queries-manager initialization
+        SamplerETLQueries samplerETLQueries = new SamplerETLQueriesManager();
+
+        // part A testing (MadaReport)
         String madaReportsSrc = configurationsHandler.getField("madaReportsSrc");
         String madaReportsDst = configurationsHandler.getField("madaReportsDst");
-
-        SamplerETLQueries samplerETLQueries = new SamplerETLQueriesManager();
         List<Entity> madaReportsList = samplerETLQueries.extractMadaReports(madaReportsSrc);
         samplerETLQueries.loadMadaReports(maxRecords, madaReportsDst, madaReportsList);
+
+        // part B testing (LabTest)
+        String labTestsSrc = configurationsHandler.getField("labTestsSrc");
+        String labTestsDst = configurationsHandler.getField("labTestsDst");
+        List<Entity> labTestsList = samplerETLQueries.extractLabTests(labTestsSrc);
     }
 }
